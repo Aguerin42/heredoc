a108459273af2fb4ed006644bdae90b0ebcdb7bc
Alexis Guérin <aguerin@student.42.fr>
2018-04-19 16:01:58 +0200

    Heredoc fonctionnel

--- src/heredoc.c
+++ src/heredoc.c
@@ -15,7 +15,6 @@ char	*find_delimiter(char *line)
 		e++;
 	if ((e - b) && !(delimiter = ft_strsub(line, b, e - b)))
 		sh_error(1, "in find_delimiter function");
-	ft_putendl(delimiter);
 	return (delimiter);
 }
 
@@ -24,39 +23,38 @@ t_lstag	*heredoc(char *line, char ** var, char **builtin)
 	char	*read;
 	char	*delimiter;
 	t_lstag	*list;
-//	t_lstag	*node;
+	t_lstag	*node;
 
 	list = NULL;
+	read = NULL;
 	if (line)
 	{
+		is_in_heredoc(1);
 		while ((line = ft_strstr(line, "<<")) && (*(line = line + 2)))
 		{
 			delimiter = find_delimiter(line);
+			redoc_delimiter(delimiter);
 			while (delimiter && !ft_strequ(read, delimiter))
 			{
-				ft_putendl("1---");
 				read ? ft_strdel(&read) : NULL;
 				ft_putchar('\n');
 				if (!(read = line_input("heredoc>", NULL, var, builtin)))
 					sh_error(1, "in heredoc function");
 				else
 				{
-				ft_putendl("2---");
-		//			if (!(node = ag_lstnew(read, ft_strlen(read) + 1)))
-		//				sh_error(1, "in heredoc function");
-		//			if (list)
-		//				ag_lstadd(&list, node);
-		//			else
-		//				list = node;
+					if (!(node = ag_lstnew(read, ft_strlen(read) + 1)))
+						sh_error(1, "in heredoc function");
+					if (list)
+						ag_lstadd(&list, node);
+					else
+						list = node;
 				}
 			}
-				ft_putendl("3---");
 			read ? ft_strdel(&read) : NULL;
-				ft_putendl("4---");
 			delimiter ? ft_strdel(&delimiter) : NULL;
-				ft_putendl("5---");
 		}
+		is_in_heredoc(1);
+		redoc_delimiter("");
 	}
-				ft_putendl("6---");
 	return (list);
 }
